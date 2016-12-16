cdba85dc3adce228a4d195c06259108050ae85fb
myungok-star <myungok.star@gmail.com>
2016-12-16 13:49:57 -0800

    added seed data, installed gem ffaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+Thought.destroy_all
+User.destroy_all
+
+p "Hello from seed.rb"
+
+#Start timing....
+startTiming = Time.now
+
+5.times do
+  email = FFaker::Internet.email
+  password = FFaker::Internet.password
+  phone = FFaker::PhoneNumber.short_phone_number
+
+  description = FFaker::Lorem.paragraph
+  category = FFaker::Book.genre
+
+  User.create(email: email, password: password, phone: phone)
+  Thought.create(description: description, category: category)
+end
+
+# End timining...
+endTiming = Time.now
+total = endTiming - startTiming
+p total
+
+p "Created #{Thought.count} thoughts"
+p "Created #{User.count} users"
